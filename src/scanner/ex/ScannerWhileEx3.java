b4defb9677738a4efbdd9aca81f625008172d193
Gorush116 <gorush34@gmail.com>
2024-01-09 08:54:50 +0900

    scanner commit4

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,43 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+        int num = 0;
+
+        System.out.println("숫자를 입력하세요 입력을 중단하려면 -1을 입력하세요.");
+
+//        while (true) {
+//
+//            num = sc.nextInt();
+//
+//            if(num == -1) {
+//                double average = (double) sum / count;
+//                System.out.println("입력한 숫자들의 합계 : " + sum);
+//                System.out.println("입력한 숫자들의 평균 : " + average);
+//                break;
+//            }
+//
+//            sum += num;
+//            count++;
+//
+//        }
+
+        while ((num = sc.nextInt()) != -1) {
+            sum += num;
+            count++;
+        }
+
+        double average = (double) sum / count;
+        System.out.println("입력한 숫자들의 합계 : " + sum);
+        System.out.println("입력한 숫자들의 평균 : " + average);
+
+    }
+}
