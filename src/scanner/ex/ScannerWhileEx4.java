cb60378a6faa000808d6533072ce9a6fff977106
Gorush116 <gorush34@gmail.com>
2024-01-10 08:35:31 +0900

    scanner commit5

---
+++ src/scanner/ex/ScannerWhileEx4.java
@@ -0,0 +1,45 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int option = 0;
+        String product = "";
+        int price = 0;
+        int quantity = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("1 : 상품 입력, 2 : 결제, 3 : 프로그램 종료");
+            option = sc.nextInt();
+            sc.nextLine();
+
+            if (option == 1) {
+                System.out.print("상품을 입력하세요 : ");
+                product = sc.nextLine();
+                System.out.print("상품의 가격을 입력하세요 : ");
+                price = sc.nextInt();
+                sc.nextLine();
+                System.out.print("구매 수량을 입력하세요 : ");
+                quantity = sc.nextInt();
+                sc.nextLine();
+                sum += price * quantity;
+                System.out.println("상품명 : " + product + ", 가격 : " + price + ", 수량 : " + quantity + ", 합계 : " + sum);
+            } else if (option == 2) {
+                System.out.println("총 비용 : " + sum);
+                sum = 0;
+            } else if (option == 3){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else {
+                System.out.println("올바른 옵션을 선택해주세요.");
+            }
+
+        }
+
+    }
+}
